897f71ae8ed13e800667f9b0ae5c95d00eb13041
EC2 Default User <106435322+show->
2022-06-11 00:16:41 +0000

    3rd

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,33 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to ''
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  private
+  def list_params
+    params.require(:book).permit(:title, :body)
   end
 end
