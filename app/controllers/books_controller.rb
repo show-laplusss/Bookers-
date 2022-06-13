8c83b7b3cba367c334342a0e9cabe7adde69726f
EC2 Default User <106435322+show->
2022-06-13 10:08:44 +0000

    [vard]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,19 @@
 class BooksController < ApplicationController
-  
-  def new
-    @book = Book.new
-  end
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
+ 
 
   def index
     @books = Book.all
+     @book = Book.new
+  end
+  def create
+    @book = Book.new(book_params)
+    if  @book.save
+       redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+   
   end
 
   def show
@@ -22,15 +25,18 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path(book.id)
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path(@book.id)
   end
 
   private
