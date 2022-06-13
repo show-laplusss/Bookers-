944a55d6d7f355cf50a151f81984afee1a9ee160
EC2 Default User <106435322+show->
2022-06-13 06:59:45 +0000

    [page]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
+  
   def new
     @book = Book.new
   end
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to ''
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -26,8 +27,14 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
   private
-  def list_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
--- db/migrate/20220610222811_add_title_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToBooks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :books, :name, :string
-  end
-end
